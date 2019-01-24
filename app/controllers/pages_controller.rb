961cbece6931f701ed2fd5600c08f05731234476
Jasper <jasper@jasperPro.local>
2019-01-24 15:57:59 +0100

    switch to travel blog

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
-  def index
+  def vietnam
   end
+
+  def japan
+  end
+
+  def mexico
+  end
+
+  def colombia
+  end
+
+  def morocco
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-	root "pages#index"
+	get "/", to: "pages#vietnam", as: :vietnam
+	get '/japan', to: "pages#japan", as: :japan
+	get '/colombia', to: "pages#colombia", as: :colombia
+	get '/morocco', to: "pages#morocco", as: :morocco
+	get '/mexico', to: "pages#mexico", as: :mexico
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
